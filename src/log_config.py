d567ddef94fd58df60171669204c1dcfc00a7b40
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2024-04-13 21:33:53 -0400

    Add return statement to configure_logging function
    The configure_logging function in log_config.py has been updated. It will now return the logger object after configuration. This change allows other modules to leverage the configured logger directly when this function is called.

--- src/log_config.py
+++ src/log_config.py
@@ -2,7 +2,6 @@ import yaml
 import logging.config
 import os
 
-
 def configure_logging():
     try:
         directory = os.path.dirname(os.path.realpath(__file__))
@@ -10,6 +9,7 @@ def configure_logging():
         with open(yaml_file_path, 'r') as f:
             config = yaml.safe_load(f.read())
             logging.config.dictConfig(config)
+        return logging.getLogger(__name__)
     except IOError:
         print('Error: File does not exist')
     except yaml.YAMLError:
